368f1ad8588229070587ffd2cda8fed315112467
awkyu <alexanderkyu@gmail.com>
2023-09-12 11:34:25 -0400

    actually open to work, lmao

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I’m currently looking for new opportunities, so please feel free to reach out via email,
+        LinkedIn, or other social media platforms!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
